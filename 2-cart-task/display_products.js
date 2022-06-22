d150222dd16027f021a581036b192180649e102c
Mohammad Talaat <eng.muhamedtalaat@gmail.com>
2022-06-22 19:10:53 +0200

    fetch data dynamically

--- 2-cart-task/display_products.js
+++ 2-cart-task/display_products.js
@@ -1,9 +1,24 @@
+
 async function fetchData(){
+    const displayedItemContainer = document.querySelector('.displayedItemContainer')
     const data = await fetch('./productsDB.json')
-    const parsedData = await data.json()
-    console.log(parsedData)
-
+    const products = await data.json()
+    displayedItemContainer.innerHTML=''
+    for (const product of products) {
+        displayedItemContainer.innerHTML += `
+      
+          <div class="card col p-3 m-2" style="width: 16rem;">
+            <img src="${product.image}" style="width:200px;height:200px;">
+            <div class="card-body">
+              <h5 class="card-title">${product.title}</h5>
+              <p class="card-text">${product.category}</p>
+              <p class="card-text">EGP ${product.price * 16}</p>
+              <button  class="btn btn-primary">Add To Cart</button>
+            </div>
+          </div>
+          
+        `
+    }
+    
 }
- fetchData()   
-
- 
+fetchData()
